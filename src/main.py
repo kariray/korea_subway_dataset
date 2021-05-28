c0fc4c477f9f672c7ec8b91b2430256061b17e4c
kariray <kari_578@naver.com>
2021-05-28 18:12:55 +0900

    Updated Code

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ from scrapper_seoul_VER_2 import get_data_seoul
 from scrapper_pusan import get_data_pusan
 from scrapper_daegu import get_data_daegu
 from scrapper_gwangju import get_data_gwangju
+from scrapper_daejeon import get_data_daejeon
 
 # get_data_seoul()
 # get_data_pusan()
 # get_data_daegu()
-get_data_gwangju()
+# get_data_gwangju()
+get_data_daejeon()
