f11674b1541247ba15964d11238b065782d235ad
kariray <kari_578@naver.com>
2021-05-28 18:06:33 +0900

    Updated Code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from scrapper_seoul_VER_2 import get_data_seoul
 from scrapper_pusan import get_data_pusan
 from scrapper_daegu import get_data_daegu
+from scrapper_gwangju import get_data_gwangju
 
 # get_data_seoul()
 # get_data_pusan()
-get_data_daegu()
+# get_data_daegu()
+get_data_gwangju()
--- src/make_csv.py
+++ src/make_csv.py
@@ -4,12 +4,12 @@ import csv
 def write_csv(line_data, city_name):
     file = open(f"dataset/{city_name}/{line_data['line_name']}.csv", mode="w")
     writer = csv.writer(file)
-    if city_name == "seoul" or city_name == "daegu":
-        writer.writerow(["number", "line", "name", "english_name", "hanja_name",
-                         "transfer", "distance", "cumalative_distance", "location"])
-    elif city_name == "pusan":
+    if city_name == "pusan":
         writer.writerow(["number", "line", "name", "english_name", "japanese_name",
                          "chinese_name", "hanja_name", "transfer", "distance", "cumulative_distance", "location"])
+    else:
+        writer.writerow(["number", "line", "name", "english_name", "hanja_name",
+                         "transfer", "distance", "cumalative_distance", "location"])
     for d in line_data['station_data']:
         writer.writerow(d.values())
     print(f"Created {line_data['line_name']}.csv file")
